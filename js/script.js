f5637d96acec28db7958bef7738bbffdefd305b9
Nemanja Djordjevic <nemanja2014@live.com>
2023-09-10 19:35:42 +0200

    version-1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("toTop");
+//  change navbar and toTopButton class on scroll
+function useScroll() {
+  const navbar = document.querySelector(".navbar");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      navbar.classList.replace("position-absolute", "navbar-sticy");
+      navbar.classList.replace("bg-opacity-10", "bg-opacity-50");
+      button.classList.add("show");
+    } else {
+      navbar.classList.replace("navbar-sticy", "position-absolute");
+      button.classList.remove("show");
+      navbar.classList.replace("bg-opacity-50", "bg-opacity-10");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", useScroll);
